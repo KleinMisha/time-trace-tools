1ef0f8f73be4e9da203570daee6b07cc22c313a0
MishaKlein <klein.misha@gmail.com>
2025-01-05 01:28:28 +0100

    A generic TimeTrace object that has a time array, any number of value arrays of equal length. Also has optional lists of labels (per section). Created some simple methods for updating these lists.

---
+++ data_types/time_trace.py
@@ -0,0 +1,86 @@
+"""
+Core class to define a single (time-) trace
+(In principle not restricted to data VS time and could be force VS extention etc.)
+"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+import numpy as np
+
+
+# custom error to improve readibility a bit
+class NotATimeTraceError(Exception):
+    pass
+
+
+@dataclass
+class TimeTrace(ABC):
+    """
+    Defines a generic time trace.
+    Abstract base class, so still needs specific implementations
+    """
+
+    ID: str
+    t: np.ndarray
+    labels: list[str] = field(default_factory=list)
+    section_labels: dict[tuple[int, int], list[str]] = field(default_factory=dict)
+
+    @property
+    @abstractmethod
+    def _values(self) -> tuple[np.ndarray]:
+        """
+        return all the value arrays as a tuple.
+        See subclasses for specific implementation
+        """
+
+    def _validate_array_lengths(self) -> None:
+        for i, value_array in enumerate(self._values):
+            if not len(value_array) == len(self.t):
+                raise NotATimeTraceError(
+                    f"All values must have the same length as t. Expected {len(self.t)}, but {i}-th value has length of {len(value_array)}"
+                )
+
+    def __post_init__(self) -> None:
+        """
+        this function will automatically be excecuted when creating a TimeTrace (or an instance of a class inheriting from it).
+        dataclasses automatically creates things like the `__init__()` method. Hence, if you want something to happen straight after that
+        you use the `__post_init__()` method when working with dataclasses.
+        """
+        _ = self._values  # make sure to instaciate this attribute
+        self._validate_array_lengths()
+
+    def __len__(self) -> int:
+        """overwrite the 'len()' method, so that when asking for the length of a time trace, it returns the number of time frames"""
+        return len(self.t)
+
+    def add_labels(self, labels: list[str]) -> None:
+        self.labels.extend(labels)
+
+    def remove_labels(self, labels: list[str]) -> None:
+        for lbl in labels:
+            self.labels.remove(lbl)
+
+    def add_labelled_section(
+        self, start_index: int, end_index: int, label: str
+    ) -> None:
+        key = (start_index, end_index)
+        if key not in self.section_labels:
+            # if this is the first time labelling this section
+            new_section = {key: [label]}
+            self.section_labels.update(new_section)
+        else:
+            # simply append to the list
+            self.section_labels[key].append(label)
+
+    def remove_all_labels_from_section(self, start_index: int, end_index: int) -> None:
+        key = (start_index, end_index)
+        self.section_labels.pop(key)
+
+    def remove_all_sections_by_label(self, label: str) -> None:
+        after_deletion = {
+            (start_index, end_index): values
+            for (start_index, end_index), values in self.section_labels.items()
+            if values.count(label) == 0
+        }
+        self.section_labels = after_deletion
