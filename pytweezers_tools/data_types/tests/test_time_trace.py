4c7dc3503dc30458db70928ac19c02fdc7709948
MishaKlein <klein.misha@gmail.com>
2025-03-17 00:06:33 +0100

    Now use the MockTimeTrace

--- pytweezers_tools/data_types/tests/test_time_trace.py
+++ pytweezers_tools/data_types/tests/test_time_trace.py
@@ -5,21 +5,21 @@ test generic workings of TimeTrace class, using MagneticTweezersTrace as an inst
 import numpy as np
 import pytest
 from magnetic_tweezers_trace import MagneticTweezersTrace
-from time_trace import InvalidTimeTraceError, TimeTrace
+from mock_time_trace import MockTimeTrace
+from exception_definitions import InvalidTimeTraceError
 
 Scalar = int | float | np.integer | np.floating
 
 
 @pytest.fixture
-def time_trace() -> MagneticTweezersTrace:
+def time_trace() -> MockTimeTrace:
     """
-    generate a mock MagneticTweezersTrace
+    generate a mock (MagneticTweezers)TimeTrace
     """
     t = np.linspace(0, 100, 10, dtype=np.float64)
     x = np.array([1.0] * len(t))
     y = np.array([1.0] * len(t))
-    z = np.array([1.0] * len(t))
-    return MagneticTweezersTrace(ID="mock", t=t, x=x, y=y, z=z)
+    return MockTimeTrace(ID="mock", t=t, value_one=x, value_two=y)
 
 
 @pytest.fixture
@@ -27,7 +27,7 @@ def scalar() -> Scalar:
     return 10.0
 
 
-def test_creating_valid_mt_trace(time_trace: TimeTrace) -> None:
+def test_creating_valid_mt_trace(time_trace: MockTimeTrace) -> None:
     print(time_trace)
 
 
@@ -43,11 +43,11 @@ def test_creating_invalid_mt_trace() -> None:
         MagneticTweezersTrace(ID, t=t, x=x, y=y, z=z)
 
 
-def test_len_of_time_trace(time_trace: TimeTrace) -> None:
+def test_len_of_time_trace(time_trace: MockTimeTrace) -> None:
     assert len(time_trace) == len(time_trace.t)
 
 
-def test_addition(time_trace: TimeTrace) -> None:
+def test_addition(time_trace: MockTimeTrace) -> None:
     """
     test that if I add a time trace to itself, all values get doubled
     """
@@ -56,7 +56,7 @@ def test_addition(time_trace: TimeTrace) -> None:
         assert np.all(new_values == original_values + original_values)
 
 
-def test_subtraction(time_trace: TimeTrace) -> None:
+def test_subtraction(time_trace: MockTimeTrace) -> None:
     """
     test that if I subtract a trace from itself, all values are zeros
     """
@@ -65,7 +65,7 @@ def test_subtraction(time_trace: TimeTrace) -> None:
         assert all(value_array == 0.0)
 
 
-def test_addition_of_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
+def test_addition_of_constant(time_trace: MockTimeTrace, scalar: Scalar) -> None:
     """
     check that I can properly add a constant value to all values in the trace
     """
@@ -74,7 +74,7 @@ def test_addition_of_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
         assert np.all(new_values == original_values + scalar)
 
 
-def test_subtraction_of_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
+def test_subtraction_of_constant(time_trace: MockTimeTrace, scalar: Scalar) -> None:
     """
     check that I can properly add a constant value to all values in the trace
     """
@@ -83,7 +83,7 @@ def test_subtraction_of_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
         assert np.all(new_values == original_values - scalar)
 
 
-def test_mupltiplication_by_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
+def test_mupltiplication_by_constant(time_trace: MockTimeTrace, scalar: Scalar) -> None:
     """
     test multiplying all values by a constant value
     """
@@ -92,7 +92,7 @@ def test_mupltiplication_by_constant(time_trace: TimeTrace, scalar: Scalar) -> N
         assert np.all(new_values == original_values * scalar)
 
 
-def test_division_by_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
+def test_division_by_constant(time_trace: MockTimeTrace, scalar: Scalar) -> None:
     """
     test dividing all values by a constant value
     """
@@ -101,46 +101,46 @@ def test_division_by_constant(time_trace: TimeTrace, scalar: Scalar) -> None:
         assert np.all(new_values == original_values / scalar)
 
 
-def test_creating_mt_trace_with_label(time_trace: TimeTrace) -> None:
+def test_creating_mt_trace_with_label(time_trace: MockTimeTrace) -> None:
     my_label = ["interesting trace"]
     time_trace.add_labels(my_label)
     assert time_trace.labels == my_label
 
 
-def test_adding_labels_to_mt_trace(time_trace: TimeTrace) -> None:
+def test_adding_labels_to_mt_trace(time_trace: MockTimeTrace) -> None:
     my_label = ["first label"]
     time_trace.add_labels(my_label)
     time_trace.add_labels(["second label", "third label"])
     assert time_trace.labels == ["first label", "second label", "third label"]
 
 
-def test_remove_label_mt_trace(time_trace: TimeTrace) -> None:
+def test_remove_label_mt_trace(time_trace: MockTimeTrace) -> None:
     my_labels = ["first label", "second label", "third label"]
     time_trace.add_labels(my_labels)
     time_trace.remove_labels(["second label"])
     assert time_trace.labels == ["first label", "third label"]
 
 
-def test_add_labelled_section(time_trace: TimeTrace) -> None:
+def test_add_labelled_section(time_trace: MockTimeTrace) -> None:
     my_sections = {(0, 3): ["first label"]}
     time_trace.add_labelled_section(start_index=0, end_index=3, label="first label")
     assert time_trace.section_labels == my_sections
 
 
-def test_add_new_label_to_existing_section(time_trace: TimeTrace) -> None:
+def test_add_new_label_to_existing_section(time_trace: MockTimeTrace) -> None:
     my_sections = {(0, 3): ["first label", "second label"]}
     time_trace.add_labelled_section(start_index=0, end_index=3, label="first label")
     time_trace.add_labelled_section(start_index=0, end_index=3, label="second label")
     assert time_trace.section_labels == my_sections
 
 
-def test_add_labelled_sections_from_dictionary(time_trace: TimeTrace) -> None:
+def test_add_labelled_sections_from_dictionary(time_trace: MockTimeTrace) -> None:
     my_sections = {(0, 3): ["first label", "second label"]}
     time_trace.add_labelled_sections_from_dictionary(my_sections)
     assert time_trace.section_labels == my_sections
 
 
-def test_add_duplicate_labels(time_trace: TimeTrace) -> None:
+def test_add_duplicate_labels(time_trace: MockTimeTrace) -> None:
     my_sections = {(0, 3): ["first label", "second label"]}
     time_trace.add_labelled_sections_from_dictionary(my_sections)
     # the .add_labelled_section() should prevent adding sections to a trace that already exist
@@ -148,7 +148,7 @@ def test_add_duplicate_labels(time_trace: TimeTrace) -> None:
     assert time_trace.section_labels == my_sections
 
 
-def test_remove_label_from_section(time_trace: TimeTrace) -> None:
+def test_remove_label_from_section(time_trace: MockTimeTrace) -> None:
     before = {
         (0, 3): ["first label"],
         (4, 6): ["first label", "second label"],
@@ -168,7 +168,7 @@ def test_remove_label_from_section(time_trace: TimeTrace) -> None:
     assert time_trace.section_labels == after
 
 
-def test_remove_all_sections_by_label(time_trace: TimeTrace) -> None:
+def test_remove_all_sections_by_label(time_trace: MockTimeTrace) -> None:
     before = {
         (0, 3): ["first label"],
         (4, 6): ["first label", "second label"],
@@ -183,7 +183,7 @@ def test_remove_all_sections_by_label(time_trace: TimeTrace) -> None:
     assert time_trace.section_labels == after
 
 
-def test_remove_section(time_trace: TimeTrace) -> None:
+def test_remove_section(time_trace: MockTimeTrace) -> None:
     before = {
         (0, 3): ["first label"],
         (4, 6): ["first label", "second label"],
@@ -199,7 +199,7 @@ def test_remove_section(time_trace: TimeTrace) -> None:
     assert time_trace.section_labels == after
 
 
-def test_fetch_indices_section_by_label(time_trace: TimeTrace) -> None:
+def test_fetch_indices_section_by_label(time_trace: MockTimeTrace) -> None:
     my_sections = {
         (0, 3): ["first label"],
         (4, 6): ["first label", "second label"],
@@ -220,7 +220,7 @@ def test_fetch_indices_section_by_label(time_trace: TimeTrace) -> None:
     )
 
 
-def test_fetch_section_non_existing_label(time_trace: TimeTrace) -> None:
+def test_fetch_section_non_existing_label(time_trace: MockTimeTrace) -> None:
     my_sections = {
         (0, 3): ["first label"],
         (4, 6): ["first label", "second label"],
