573b943465daf1bc5c3b27ca404ed3293fac7197
MishaKlein <klein.misha@gmail.com>
2025-07-04 16:17:31 +0200

    Moved common logic of any Filter Transformation into ABC. Avoids duplicate code. Allows for simple unit tests. tests passed.

--- src/data_processing/filtering.py
+++ src/data_processing/filtering.py
@@ -4,6 +4,7 @@ Common filter Transformations
 -- Misha, June 2025
 """
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional
 
@@ -11,26 +12,76 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.signal import filtfilt, firwin, kaiserord
 
+from src.data_processing.transformation import Transformation
 from src.data_types.type_definitions import TimeTraceType
 
 
 @dataclass
-class KaiserBesselFilter:
+class Filter(ABC):
+    """A generic Filter Transformation. Gather common functionality in here to avoid duplicate code/ allow for simple testing."""
+
+    target_traces: list[str]
+    coordinate: str
+
+    @abstractmethod
+    def filter(
+        self, time: NDArray[np.floating], raw_signal: NDArray[np.floating]
+    ) -> tuple[NDArray[np.floating], NDArray[np.floating]]:
+        """Implement the specific filtering operation here. Return (time, filtered_signal)"""
+
+    def _traces_have_coordinate(
+        self, trace_list: list[TimeTraceType]
+    ) -> tuple[bool, Optional[str]]:
+        """check if the coordinate is valid for the given target traces. If not, it will return the first trace ID at which the check fails"""
+        targets = [trace for trace in trace_list if trace.ID in self.target_traces]
+        for target_trace in targets:
+            if self.coordinate not in target_trace._value_names:
+                return False, target_trace.ID
+        return True, None
+
+    def apply(self, trace_list: list[TimeTraceType]) -> list[TimeTraceType]:
+        """create filtered traces"""
+
+        # Validate coordinate exists for all traces
+        valid_coordinate, invalid_trace = self._traces_have_coordinate(trace_list)
+        if not valid_coordinate:
+            raise AttributeError(
+                f"Trace {invalid_trace} does not have a value-array named {self.coordinate}"
+            )
+
+        # Loop over trace list and apply filter. Always the same code, independent of the kind of filter
+        new_traces = []
+        for trace in trace_list:
+            if trace.ID in self.target_traces:
+                time = trace.t
+                value_array = trace.__getattribute__(self.coordinate)
+                filtered_time, filtered_values = self.filter(time, value_array)
+                filtered_trace = trace
+                filtered_trace.__setattr__("t", filtered_time)
+                filtered_trace.__setattr__(self.coordinate, filtered_values)
+                new_traces.append(filtered_trace)
+            else:
+                # keep the other traces unaffected
+                new_traces.append(trace)
+
+        return new_traces
+
+
+@dataclass
+class KaiserBesselFilter(Filter):
     """
     Kaiser-Bessel (low-pass) filter.
     ---
     Typical filter used for polymerase primer-extension traces.
     """
 
-    target_traces: list[str]
-    coordinate: str
     acquisition_frequency: float = 58.0
     cutoff_frequency: float = 2.0
     transition_width: float = 0.01  # relative to nyquist frequency. So width in Hz = width * acquisition_frequency /2
     stopband_attenuation_dB: float = 100
 
     def filter(
-        self, time: NDArray[np.floating], signal: NDArray[np.floating]
+        self, time: NDArray[np.floating], raw_signal: NDArray[np.floating]
     ) -> tuple[NDArray[np.floating], NDArray[np.floating]]:
         """apply the Kaiser-Bessel filter to a single time trace. "signal" is the value array named self.coordinate."""
         # Create Finite Impulse Response (FIR) filter window. Kaiser-Bessel window of order N and with parameter beta.
@@ -43,46 +94,5 @@ class KaiserBesselFilter:
         )
 
         # use the filter (NOTE: the `filtfilt` function undoes the delay created by the `lfilter` function we used previously)
-        filtered_signal: np.ndarray = filtfilt(fir_coefficients, 1.0, signal)
+        filtered_signal: np.ndarray = filtfilt(fir_coefficients, 1.0, raw_signal)
         return time, filtered_signal
-
-    # todo: Take care of duplicate code?
-    # ? make some 'subtype' of Transformation that acts on a given coordinate? Inherit from Transformation?
-    def _traces_have_coordinate(
-        self, trace_list: list[TimeTraceType]
-    ) -> tuple[bool, Optional[str]]:
-        """check if the coordinate is valid for the given target traces. If not, it will return the first trace ID at which the check fails"""
-        targets = [trace for trace in trace_list if trace.ID in self.target_traces]
-        for target_trace in targets:
-            if self.coordinate not in target_trace._value_names:
-                return False, target_trace.ID
-
-        return True, None
-
-    def apply(self, trace_list: list[TimeTraceType]) -> list[TimeTraceType]:
-        """create filtered traces"""
-        # ? Possibly remove this check? Now there is duplicate code
-        # todo: take care of duplicate code?
-        valid_coordinate, invalid_trace = self._traces_have_coordinate(trace_list)
-        if not valid_coordinate:
-            raise AttributeError(
-                f"Trace {invalid_trace} does not have a value-array named {self.coordinate}"
-            )
-
-        new_traces = []
-        for trace in trace_list:
-            if trace.ID in self.target_traces:
-                time = trace.t
-                value_array = trace.__getattribute__(self.coordinate)
-                filtered_time, filtered_values = self.filter(time, value_array)
-
-                #!CHECK THAT NO COPYING IS NEEDED AS THE EXPERIMENTPROCESSOR TAKES CARE OF THIS. IF NOT TRUE, ADD DEEPCOPY
-                filtered_trace = trace
-                filtered_trace.__setattr__("t", filtered_time)
-                filtered_trace.__setattr__(self.coordinate, filtered_values)
-                new_traces.append(filtered_trace)
-            else:
-                # keep the other traces unaffected
-                new_traces.append(trace)
-
-        return new_traces
--- tests/data_processing/test_filtering.py
+++ tests/data_processing/test_filtering.py
@@ -1,227 +1,98 @@
 """
-First write unit tests outside the context of the ExperimentProcessor.
-todo: Some elegant way of having checks on coordinates and applying operations only to desired traces in one place in code.
-!  Didn't test that again here as data_processing/test_processor.py already took care of testing this once.
-? Having some way of removing duplicate code would also allow to test it at one place.
+Test the common logic implemented in the Filter class
 """
-
-from itertools import product
 
 import numpy as np
 import pytest
 from numpy.typing import NDArray
-from scipy.fft import fft, fftfreq
 
-from src.data_processing.filtering import KaiserBesselFilter
+from src.data_processing.filtering import Filter
 from src.data_processing.processor import ExperimentProcessor
 from tests.data_types.mock_experiment import (
     MockExperiment,
+    create_mock_dataset,
     parse_mock_dataset,
 )
-from tests.data_types.mock_time_trace import MockTimeTrace
 
-MockSignal = tuple[NDArray[np.floating], NDArray[np.floating], float, float]
-
-
-# define simple constant here
-ACQUISITION_FREQUENCY = 58.0
-DURATION = 1800.0
-TIME_POINTS = int(DURATION * ACQUISITION_FREQUENCY)
-NUMBER_OF_TRACES = 100
+NUMBER_MOCK_TRACES = 100
 
 
-def create_mock_signal(
-    cutoff_frequency: float, transition_width: float = 5.0
-) -> MockSignal:
-    """Define a simple signal composed of two sine waves of different frequencies"""
-    # create a signal with two frequency components, one inside the passband, one inside the stopband (outside the passband)
-    t = np.linspace(0, DURATION, TIME_POINTS, endpoint=False)
-    freq_pass = cutoff_frequency * 0.5
-    freq_stop = cutoff_frequency + 2.0 * transition_width
-    sine_pass = np.sin(2 * np.pi * freq_pass * t)
-    sine_stop = np.sin(2 * np.pi * freq_stop * t)
-    x = sine_pass + sine_stop
-    return t, x, freq_pass, freq_stop
-
-
-def calculate_amplitude(x: NDArray[np.floating], freq: float) -> float:
-    """Use FFT to determine amplitude at (around) specified frequency"""
-    # Use |X(f)| as the amplitude of frequency component f. With X(f) the Fourier transform of x(t)
-    fft_x = np.asarray(fft(x))
-    frequencies = fftfreq(len(x), d=1 / int(ACQUISITION_FREQUENCY))
-
-    # Normalize to get amplitude spectrum
-    amplitude_spectrum = np.abs(fft_x) / len(x)
-
-    # get the amplitude at sampled frequency closest to target
-    idx_freq = np.argmin(np.abs(frequencies - freq))
-    return amplitude_spectrum[idx_freq]
-
-
-def calculate_attenuation(
-    x: NDArray[np.floating], freq_pass: float, freq_stop: float
-) -> float:
-    """Use |X(freq_stop)|/|X(freq_pass)| to determine attenuation in decibel"""
-    # Check frequency spectrum of filtered signal using FFT
-    amplitude_stopband = calculate_amplitude(x, freq_stop)
-    amplitude_passband = calculate_amplitude(x, freq_pass)
-
-    # Determine attenuation and convert into decibel
-    attenuation_dB = 20 * np.log10(amplitude_stopband / amplitude_passband)
-    return attenuation_dB
-
-
-def create_mock_experiment(
-    cutoff_frequency: float,
-    transition_width: float = 0.01 * 0.5 * ACQUISITION_FREQUENCY,
-) -> MockExperiment:
+@pytest.fixture
+def experiment(number_of_traces: int = NUMBER_MOCK_TRACES) -> MockExperiment:
     """
-    Create a mock experiment with signals as mock_signals (sine waves, to be used for testing filters)
+    generate a mock Experiment
     """
     exp = MockExperiment(ID="mock")
-    t, signal, *_ = create_mock_signal(cutoff_frequency, transition_width)
-    x = signal.copy()
-    y = signal.copy()
-    mock_traces = [
-        MockTimeTrace(ID="mock", t=t, value_one=x, value_two=y)
-        for _ in range(NUMBER_OF_TRACES)
-    ]
-
-    data_table = [t]
-    for trace in mock_traces:
-        data_table.append(trace.value_one)
-        data_table.append(trace.value_two)
-
-    data = np.array(data_table).T
+    data = create_mock_dataset(number_traces=number_of_traces)
     one, two, time = parse_mock_dataset(data)
     exp._raw_data = one, two, time
     exp.create_traces_from_raw_data()
     return exp
 
 
-@pytest.mark.parametrize(
-    "cutoff_frequency, attenuation_factor",
-    list(product([0.5, 1.0, 2.0], [10, 100])),
-)
-def test_kaiser_bessel_output_size(
-    cutoff_frequency: float, attenuation_factor: float
-) -> None:
-    """
-    check that filtered signal has the same length as the raw signal.
-    NOTE: SciPy filters perform convolution after some zero-padding, and then crop the final result to return a signal of the same length as the original.
-    """
-    time_before, signal_before, *_ = create_mock_signal(cutoff_frequency)
-    acq_freq = ACQUISITION_FREQUENCY
-    kb_filter = KaiserBesselFilter(
-        target_traces=[""],
-        coordinate="",
-        acquisition_frequency=acq_freq,
-        cutoff_frequency=cutoff_frequency,
-    )
+class MockFilter(Filter):
+    """Dummy filter operation that turns the entire signal into 42's"""
 
-    time_after, signal_after = kb_filter.filter(time_before, signal_before)
-
-    assert len(time_before) == len(time_after)
-    assert len(signal_before) == len(signal_after)
+    def filter(
+        self, time: NDArray[np.floating], raw_signal: NDArray[np.floating]
+    ) -> tuple[NDArray[np.floating], NDArray[np.floating]]:
+        """all values become 42"""
+        filtered_signal = np.array([42.0] * len(raw_signal))
+        return time, filtered_signal
 
 
-@pytest.mark.parametrize(
-    "cutoff_frequency, attenuation_factor",
-    list(product([0.5, 1.0, 2.0], [10, 100])),
-)
-def test_kaiser_bessel_attenuation_at_high_frequencies(
-    cutoff_frequency: float, attenuation_factor: float
-) -> None:
-    """
-    Check that filtered signal has less energy at a frequency in stopband compared to passband
-    """
-    t, x, freq_pass, freq_stop = create_mock_signal(cutoff_frequency)
-    acq_freq = ACQUISITION_FREQUENCY
-    kb_filter = KaiserBesselFilter(
-        target_traces=[""],
-        coordinate="",
-        acquisition_frequency=acq_freq,
-        cutoff_frequency=cutoff_frequency,
-    )
-
-    _, x_filtered = kb_filter.filter(t, x)
-    attenuation_dB = calculate_attenuation(x_filtered, freq_pass, freq_stop)
-    assert attenuation_dB < -attenuation_factor + 5.0
-
-
-@pytest.mark.parametrize(
-    "cutoff_frequency, attenuation_factor",
-    list(product([0.5, 1.0, 2.0], [10, 100])),
-)
-def test_kaiser_bessel_attenuation_wrt_original(
-    cutoff_frequency: float, attenuation_factor: float
-) -> None:
-    """
-    Check if the filter actually attenuates the signal above the cutoff when compared to the raw signal.
-    """
-    # create raw signal
-    t, x_raw, freq_pass, freq_stop = create_mock_signal(cutoff_frequency)
-    acq_freq = ACQUISITION_FREQUENCY
-    kb_filter = KaiserBesselFilter(
-        target_traces=[""],
-        coordinate="",
-        acquisition_frequency=acq_freq,
-        cutoff_frequency=cutoff_frequency,
-    )
-
-    # filter the signal
-    _, x_filtered = kb_filter.filter(t, x_raw)
-
-    # check that the filter actually did its job correctly
-    attenuation_before = calculate_attenuation(x_raw, freq_pass, freq_stop)
-    assert attenuation_before >= -attenuation_factor + 5.0
-    attenuation_after = calculate_attenuation(x_filtered, freq_pass, freq_stop)
-    assert (attenuation_after - attenuation_before) <= -attenuation_factor + 5.0
-
-
-def test_kaiser_bessel_within_processor() -> None:
-    """
-    NOTE: Strictly no longer required as ExperimentProcessor has its own unit tests already.
-          Since this is the first serious Transformation I implementing, I wanted to perform these checks to make sure
-
-    integrated in the Experiment processor, test the following:
-    1. Does my pipeline run?
-    2. Does it correctly not overwrite the original signal?
-    """
-    # create mock data here
-    cutoff_frequency = 0.5
-    attenuation_factor = 10.0
-    mock_experiment = create_mock_experiment(cutoff_frequency, attenuation_factor)
-
-    # set up analysis
-    processor = ExperimentProcessor(mock_experiment)
+def test_only_filter_target_coordinate(experiment: MockExperiment) -> None:
+    """Verify logic will only be applied to the existing coordinate"""
+    first_half_of_traces = [f"trace_{i}" for i in range(NUMBER_MOCK_TRACES // 2)]
+    processor = ExperimentProcessor(experiment)
     processor.add_transformation(
-        KaiserBesselFilter(
-            target_traces=[trace.ID for trace in mock_experiment.traces],
+        MockFilter(
+            target_traces=first_half_of_traces,
             coordinate="value_one",
-            acquisition_frequency=ACQUISITION_FREQUENCY,
-            cutoff_frequency=cutoff_frequency,
-            stopband_attenuation_dB=attenuation_factor,
         )
     )
-
-    # perform analysis
     processor.run()
 
-    # Perform similar checks as before, just to make sure things went OK in the context of the ExperimentProcessor
-    traces_before_filtering = processor.original_experiment.traces
-    filtered_experiment = processor.get_current_experiment()
-    traces_after_filtering = filtered_experiment.traces
-    freq_pass = cutoff_frequency * 0.2
-    freq_stop = cutoff_frequency + 5.0
+    modified_experiment = processor.get_current_experiment()
+    for trace in modified_experiment.traces:
+        if trace.ID in first_half_of_traces:
+            # the modified coordinate:
+            assert all(value == 42.0 for value in trace.value_one)
+            # unmodified coordinates:
+            assert all(value == 1.0 for value in trace.value_two)
 
-    for before, after in zip(traces_before_filtering, traces_after_filtering):
-        attenuation_before = calculate_attenuation(
-            before.value_one, freq_pass, freq_stop
+
+def test_validate_coordinate(experiment: MockExperiment) -> None:
+    """Verify an error is raised when attempting to access a non-existing value array"""
+    first_half_of_traces = [f"trace_{i}" for i in range(NUMBER_MOCK_TRACES // 2)]
+    processor = ExperimentProcessor(experiment)
+    processor.add_transformation(
+        MockFilter(
+            target_traces=first_half_of_traces,
+            coordinate="value_three",
         )
-        attenuation_after = calculate_attenuation(after.value_one, freq_pass, freq_stop)
+    )
+    with pytest.raises(
+        AttributeError,
+        match="Trace trace_1 does not have a value-array named value_three",
+    ):
+        processor.run()
 
-        # Sanity check: signal before filtering should contain significant energy in stopband
-        assert attenuation_before > -attenuation_factor + 5.0
-        # Now test performance of filter
-        assert (attenuation_after - attenuation_before) <= -attenuation_factor + 5.0
+
+def test_only_filter_target_traces(experiment: MockExperiment) -> None:
+    """Traces not in the target_traces list should just get passed onwards without modification"""
+    first_half_of_traces = [f"trace_{i}" for i in range(NUMBER_MOCK_TRACES // 2)]
+    processor = ExperimentProcessor(experiment)
+    processor.add_transformation(
+        MockFilter(
+            target_traces=first_half_of_traces,
+            coordinate="value_one",
+        )
+    )
+    processor.run()
+
+    modified_experiment = processor.get_current_experiment()
+    for trace in modified_experiment.traces:
+        if trace.ID not in first_half_of_traces:
+            assert all(value == 1.0 for value in trace.value_one)
+            assert all(value == 1.0 for value in trace.value_one)
