a2be7d9792afa5459ed04e0aad0c1eb82c57667f
MishaKlein <klein.misha@gmail.com>
2025-07-19 12:41:41 +0200

    Rewrote Filter class using the BaseTransformation to avoid duplicate code

--- src/data_processing/filtering.py
+++ src/data_processing/filtering.py
@@ -4,23 +4,20 @@ Common filter Transformations
 -- Misha, June 2025
 """
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from dataclasses import dataclass
-from typing import Optional
 
 import numpy as np
 from numpy.typing import NDArray
 from scipy.signal import filtfilt, firwin, kaiserord
 
+from src.data_processing.transformation import CoordinateTransformation
 from src.data_types.type_definitions import TimeTraceType
 
 
 @dataclass
-class Filter(ABC):
+class Filter(CoordinateTransformation):
     """A generic Filter Transformation. Gather common functionality in here to avoid duplicate code/ allow for simple testing."""
-
-    target_traces: list[str]
-    coordinate: str
 
     @abstractmethod
     def filter(
@@ -28,42 +25,16 @@ class Filter(ABC):
     ) -> tuple[NDArray[np.floating], NDArray[np.floating]]:
         """Implement the specific filtering operation here. Return (time, filtered_signal)"""
 
-    def _traces_have_coordinate(
-        self, trace_list: list[TimeTraceType]
-    ) -> tuple[bool, Optional[str]]:
-        """check if the coordinate is valid for the given target traces. If not, it will return the first trace ID at which the check fails"""
-        targets = [trace for trace in trace_list if trace.ID in self.target_traces]
-        for target_trace in targets:
-            if self.coordinate not in target_trace._value_names:
-                return False, target_trace.ID
-        return True, None
-
-    def apply(self, trace_list: list[TimeTraceType]) -> list[TimeTraceType]:
-        """create filtered traces"""
-
-        # Validate coordinate exists for all traces
-        valid_coordinate, invalid_trace = self._traces_have_coordinate(trace_list)
-        if not valid_coordinate:
-            raise AttributeError(
-                f"Trace {invalid_trace} does not have a value-array named {self.coordinate}"
-            )
-
-        # Loop over trace list and apply filter. Always the same code, independent of the kind of filter
-        new_traces = []
-        for trace in trace_list:
-            if trace.ID in self.target_traces:
-                time = trace.t
-                value_array = trace.__getattribute__(self.coordinate)
-                filtered_time, filtered_values = self.filter(time, value_array)
-                filtered_trace = trace
-                filtered_trace.__setattr__("t", filtered_time)
-                filtered_trace.__setattr__(self.coordinate, filtered_values)
-                new_traces.append(filtered_trace)
-            else:
-                # keep the other traces unaffected
-                new_traces.append(trace)
-
-        return new_traces
+    def apply_to_one_trace(self, trace: TimeTraceType) -> TimeTraceType:
+        """creation of the filtered TimeTrace has shared logic independent of the actual filter operation"""
+        self.target_traces
+        time = trace.t
+        value_array = trace.__getattribute__(self.coordinate)
+        filtered_time, filtered_values = self.filter(time, value_array)
+        filtered_trace = trace
+        filtered_trace.__setattr__("t", filtered_time)
+        filtered_trace.__setattr__(self.coordinate, filtered_values)
+        return filtered_trace
 
 
 @dataclass
